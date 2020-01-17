0d4364dec31d23989ded8348e1360a9145a3c308
Saurabh.Srivastava1 <Saurabh.Srivastava1@SEZ60CIBC.india.rsystems.com>
2020-01-17 20:52:07 +0530

    comit

--- netflix-zuul-api-gateway-server/src/main/java/com/example/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/example/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -1,25 +1,24 @@
-package com.example.microservices.currencyconversionservice;
+package com.example.microservices.netflixzuulapigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
-@SpringBootApplication
-@EnableFeignClients("com.example.microservices.currencyconversionservice")
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class CurrencyConversionServiceApplication {
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
-	
+
 	@Bean
 	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
 	}
-
 }
